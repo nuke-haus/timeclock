d92c34031b72d9fc29eda069e381c785765bd51e
nukehaus <nuke.twoski@gmail.com>
2026-07-24 09:50:31 -0400

    clock keypad

--- scripts/ClockBody.jsx
+++ scripts/ClockBody.jsx
@@ -81,29 +81,31 @@ class ClockBody extends React.Component {
 
     _renderKeypad() {
         return (
-            <div className="keypadDiv">
-                <span>
-                    {this.state.code}
-                </span>
-                <div>
-                    <button onClick={() => this._onClick("1")}>1</button>
-                    <button onClick={() => this._onClick("2")}>2</button>
-                    <button onClick={() => this._onClick("3")}>3</button>
-                </div>
-                <div>
-                    <button onClick={() => this._onClick("4")}>4</button>
-                    <button onClick={() => this._onClick("5")}>5</button>
-                    <button onClick={() => this._onClick("6")}>6</button>
-                </div>
-                <div>
-                    <button onClick={() => this._onClick("7")}>7</button>
-                    <button onClick={() => this._onClick("8")}>8</button>
-                    <button onClick={() => this._onClick("9")}>9</button>
-                </div>
-                <div>
-                    <button onClick={() => this._onClickBackspace()}>🢀</button>
-                    <button onClick={() => this._onClick("0")}>0</button>
-                    <button onClick={() => this._onClickClear()}>☒</button>
+            <div className="keypadContainerDiv">
+                <div className="keypadDiv">
+                    <span>
+                        {this.state.code}
+                    </span>
+                    <div>
+                        <button onClick={() => this._onClick("1")}>1</button>
+                        <button onClick={() => this._onClick("2")}>2</button>
+                        <button onClick={() => this._onClick("3")}>3</button>
+                    </div>
+                    <div>
+                        <button onClick={() => this._onClick("4")}>4</button>
+                        <button onClick={() => this._onClick("5")}>5</button>
+                        <button onClick={() => this._onClick("6")}>6</button>
+                    </div>
+                    <div>
+                        <button onClick={() => this._onClick("7")}>7</button>
+                        <button onClick={() => this._onClick("8")}>8</button>
+                        <button onClick={() => this._onClick("9")}>9</button>
+                    </div>
+                    <div>
+                        <button onClick={() => this._onClickBackspace()}>🢀</button>
+                        <button onClick={() => this._onClick("0")}>0</button>
+                        <button onClick={() => this._onClickClear()}>☒</button>
+                    </div>
                 </div>
             </div>
         );
