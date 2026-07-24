22ecc2d67eb2dd6a7304c7984184734fc074c1c8
nukehaus <nuke.twoski@gmail.com>
2026-07-24 16:52:05 -0400

    Update DatabaseBody.jsx

--- scripts/DatabaseBody.jsx
+++ scripts/DatabaseBody.jsx
@@ -17,11 +17,46 @@ class DatabaseBody extends React.Component {
         return String(value).toUpperCase();
     }
 
+    _renderUsers() {
+        for (const [i, value] of TC.database.people.entries()) {
+            result.push(
+                <tr key={'user' + i}>
+                    <td><button>{value.name}</button></td>
+                    <td>{value.code}</td>
+                    <td>{value.activeTimeSpan != null ? "CLOCKED IN" : "-"}</td>
+                </tr>
+            );
+        }
+    }
+
+    _renderUserList() {
+        if (TC.database.people.length === 0) {
+            return null;
+        }
+        return (
+            <div>
+                <div className="tabletext">
+                    USERS TABLE
+                </div>
+                <table className="formulatablesmall">
+                    <tbody>
+                        <tr>
+                            <th>NAME</th>
+                            <th>CODE</th>
+                            <th>STATUS</th>
+                        </tr>
+                        {this._renderUsers()}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
     render() {
     
         return (
             <div>
-                <a ref={(ref) => this._downloadLink = ref}/>
+                {this._renderUserList()}
             </div>
         );
     }
