8c9933fe92df0aa3787a5d0300dddaca4c2d35c4
nukehaus <nuke.twoski@gmail.com>
2026-07-24 17:38:15 -0400

    Update Clock.js

--- scripts/Clock.js
+++ scripts/Clock.js
@@ -149,7 +149,7 @@ TC.addEffects = function() {
             rotate: (Math.random() * -6) + 3,
             size: (Math.random() * 3) + 1,
             speed: (Math.random() * 3) + 1,
-            wobble: (Math.random() * 20),
+            wobble: (Math.random() * 80),
             name: new Date().getMilliseconds() + "emoji" + i
         });
     }
